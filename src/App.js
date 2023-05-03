ce1aac85de671a2f5dc263d0bd7b98f9f47250b3
Ivan <ivanwong810520@gmail.com>
2023-05-03 09:38:07 +0800

    Created components with useState Hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ClassCounter from "./components/useState/ClassCounter";
+import ClassCounterTwo from "./components/useState/ClassCounterTwo";
+import HookCounter from "./components/useState/HookCounter";
+import HookCounterTwo from "./components/useState/HookCounterTwo";
+import HookCounterThree from "./components/useState/HookCounterThree";
+import HookCounterFour from "./components/useState/HookCounterFour";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ClassCounter />
+            <ClassCounterTwo />
+            <HookCounter />
+            <HookCounterTwo />
+            <HookCounterThree />
+            <HookCounterFour />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/useState/HookCounterTwo.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function HookCounterTwo() {
+    const initialCount = 0;
+    const [count, setCount] = useState(initialCount);
+
+    // After click the button, it still increase 1 only, need to update the value based on the previous value
+    /*
+    const incrementFive = () => {
+        for (let i = 0; i < 5; i++) {
+            setCount(count + 1);
+        }
+    };
+    */
+
+    const incrementFive = () => {
+        for (let i = 0; i < 5; i++) {
+            setCount((prevCount) => prevCount + 1);
+        }
+    };
+
+    return (
+        <div>
+            Count: {count}
+            <button onClick={() => setCount(initialCount)}>Reset</button>
+            <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+                Increment
+            </button>
+            <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+                Decrement
+            </button>
+            <button onClick={incrementFive}>Increment 5</button>
+        </div>
+    );
+}
+
+export default HookCounterTwo;
